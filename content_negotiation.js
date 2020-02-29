4420d076f08163667ec9a41a61bf709ccc857499
Ravindra Kumawat <ravindra.kumawat76@gmail.com>
2020-02-29 22:37:21 +0530

    Form Field Exercises

---
+++ content_negotiation.js
@@ -0,0 +1,14 @@
+// const url = 'https://eloquentjavascript.net/author'
+// const types = ['text/plain',
+//   'text/html',
+//   'application/json',
+//   'application/rainbows+unicorns']
+
+// async function showTypes () {
+//   for (const type of types) {
+//     const resp = await fetch(url, { headers: { accept: type } })
+//     console.log(`${type}: ${await resp.text()}\n`)
+//   }
+// }
+
+// showTypes()
--- practice.js
+++ practice.js
@@ -1,24 +1,23 @@
-// function Bird() {
-//   let arr = [];
-//   this.getWeight = function(value){
-//     arr.push(value)
-//     return arr;
-//   }
-// }
-// let bird1 = new Bird();
-// console.log(bird1.getWeight(4));
-// console.log(bird1.getWeight(5));
-
-function bird() {
- const arrr = []
- function addEle(value) {
-  arrr.push(value);
-  return arrr;
- }
- return addEle;
+const str = 'ravinnnnnnndra'
+const obj = {}
+const count = 1
+for (let i = 0; i < str.length; i++) {
+  if (obj.hasOwnProperty(str[i])) {
+    obj[str[i]]++
+  } else {
+    obj[str[i]] = count
+  }
 }
-let bird1 = bird();
-// bird1(4);
-// console.log(bird1(3))
-console.log(bird1(3))
 
+const keys = Object.keys(obj)
+const values = Object.values(obj)
+const maxValue = Math.max(...values)
+const index = values.indexOf(maxValue)
+
+console.log(str)
+console.log(obj)
+console.log(keys)
+console.log(values)
+console.log(maxValue)
+console.log(index)
+console.log(keys[index])
